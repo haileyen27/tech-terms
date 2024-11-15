1f6a686ac036f105ed05001e111effacf4248d23
Zane Long <78000694+LightningBlu@users.noreply.github.com>
2024-11-15 13:43:20 -0500

    Update login.php
    Updating for sake of documenting possible changes

--- EditorLogin/login.php
+++ EditorLogin/login.php
@@ -16,25 +16,39 @@
     </head>
 	<body>
 		<header>
-        	<h1 id="title">Tech Terms</h1>
-            	<div id="navbar">
-           	     	<button type="button" class="navbutton"><a href="../indexx.php">Home</a></button> <!-- change buttons to onclick when JS/php is integrated -H-->
+            	<h1 id="title">Tech Terms</h1>
+            		<div id="navbar">
+                	<button type="button" class="navbutton"><a href="../indexx.php">Home</a></button> <!-- change buttons to onclick when JS/php is integrated -H-->
                 	<button type="button" class="navbutton"><a href="../pagelist.html">All Pages</a></button>
-               		<form method="get" action="search.php">
-                    	<select type="button" class="navbutton" name="categories" placeholder="Categories">
-                        	<option>Categories</option><!--go to list of categories -H-->
-                        	<option>Terms</option>
-                	        <option>Locations</option>
-        	                <option>Events</option>
-	                    </select> <!-- Need to go to page on click (form submit + script go to link on option select) -H-->
-	                </form>
+                	<form method="get" action="search.php" class="form-test">
+                    		<select type ="button" class="navbutton" name="categories" placeholder="Categories" onchange="
+                    		if(this.value == 3) {
+                        		window.location.href = '../categories/events.html';
+                    		} else if (this.value == 2){
+                  		      	window.location.href = '../categories/locations.html';
+                    		} else if (this.value == 1){
+                        		window.location.href = '../categories/terms.html';
+                    		}
+                		">
+                    			<option value="0" >Categories</option>
+                    			<option value="1" >Terms</option>
+                		    	<option value="2" >Locations</option>
+                    			<option value="3" >Events</option>
+                		</select> <!-- end of dropdown -->
+                	</form>
                 	<button type="button" class="navbutton">Random</button>
                 	<div class="navbutton" style="padding:0px;border-width:0px;">
-                   		<input type="text" class="navbutton" id="searchbar" name="searchbar" placeholder="Search..."><input type="submit" class="navbutton" value="&rArr;">
+                    		<input type="text" class="navbutton" id="searchbar" name="searchbar" placeholder="Search..."><input type="submit" class="submit-button" value="&rArr;">
                     		<!--Searchbar and submit button must remain on same line or it adds a gap. Button will be connected to PHP for search functionality. -H-->
                 	</div>
-                	<button type="button" class="navbutton" id="login">Log In</button>
-            		</div>
+			<?php
+                		if(isset($SESSION["username"])){
+					?> <button type="button" class="navbutton" id="login"><a href="./EditorLogin/login.php">Log Out</a></button> <?php
+				}else{
+					?> <button type="button" class="navbutton" id="login"><a href="./EditorLogin/login.php">Log In</a></button> <?php
+				}
+            		?>
+			</div>	
         	</header>
 		<main>
 		<div class="spacer"></div>
